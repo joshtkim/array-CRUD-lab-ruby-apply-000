82b1d3cd0d394aaa77e1ddc75db12f5a4f1ab23e
Joshua Kim <jykim92@gmail.com>
2020-03-31 18:12:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  James = ["John", "Jacob", "Jimmy", "Joe"]
-  James << "Jacob"
 end
+
+
 
 def add_element_to_start_of_array(array, element)
 
